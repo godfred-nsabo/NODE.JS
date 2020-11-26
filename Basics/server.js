1a88ef95193cede7a9d7eb03599b6fa9c77dc057
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-11-26 16:37:17 +0000

    Basics - Parsing Request Bodies

--- Basics/server.js
+++ Basics/server.js
@@ -1,6 +1,7 @@
 //importing in Node.js
 const http = require("http");
-const fs = require('fs');
+const fs = require("fs");
+//const { chdir } = require("process");
 /*const rqListener = (req, res) => {
   console.log(req);
 };*/
@@ -11,20 +12,33 @@ const fs = require('fs');
 const server = http.createServer(function (req, res) {
   const url = req.url;
   const method = req.method;
-  if (url === '/'){
+  if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
-    res.write('<body><form action="/message" method="POST"><input type="text" name ="message"><button type="submit">Send</button></form></body>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name ="message"><button type="submit">Send</button></form></body>'
+    );
     res.write("</html>");
     return res.end();
-  };
-    //process.exit(); // or use CTRL + C
-   if(url ==='/message' && method === 'POST') {
-        fs.writeFileSync('message.txt', 'DEPLOY');
-        res.statusCode = 302;
-        res.setHeader('location', '/')
-        return res.end();
-   }
+  }
+  //process.exit(); // or use CTRL + C
+  if (url === "/message" && method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      console.log(parsedBody);
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
   res.setHeader("Content-Type", "text/html");
   res.write("<html>");
   res.write("<head><title>My First Page</title></head>");
@@ -32,7 +46,7 @@ const server = http.createServer(function (req, res) {
   res.write("</html>");
   res.end();
 });
-server.listen(5050);
+server.listen(5000);
 
 // or using Nextgen function
 
