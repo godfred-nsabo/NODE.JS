19171d6eb7d2c7257b5a5d8f848a45246281a1d0
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-11-26 15:52:45 +0000

    Basics - Understanding and sending  Responsse

--- Basics/server.js
+++ Basics/server.js
@@ -9,12 +9,15 @@ const http = require("http");
 // or using the anomymous function
 
 const server = http.createServer(function (req, res) {
-  console.log(req);
-  process.exit();
+  //process.exit(); // or use CTRL + C
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.Js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 server.listen(5050);
-
-
 
 // or using Nextgen function
 
