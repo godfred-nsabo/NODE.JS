a84cf897dfedcfefd3b8c8bd9d379974df4c2cd2
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-11-26 10:58:53 +0000

    JS - Working with Objects

--- js/play.js
+++ js/play.js
@@ -19,5 +19,31 @@ const add = (a, b) => {
   return a + b;
 };
 
+//simply written the arrow function as
+const sum = (x, y) => x + y;
+
+// with one argument
+const sum1 = (z) => z + 2;
+
 console.log(add(1, 2));
+console.log(sum(1, 3));
+console.log(sum1(14));
 console.log(summariseUser(name, age, hasHobbies));
+
+const person = {
+  name: "Goddey",
+  age: 29,
+  greet: () => {
+    console.log("Hello, I am " + this.name);
+  },
+  greet1: function() {
+    console.log("Hello, I am " + this.name);
+  },
+  greet2() {
+    console.log("Hello, I am " + this.name);
+  },
+};
+
+person.greet();
+person.greet1();
+person.greet2();
