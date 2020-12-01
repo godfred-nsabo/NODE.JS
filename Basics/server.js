f09782def10a5448a63cf137b1802c79e7e35592
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-12-01 10:16:31 +0000

    Using Node module system

--- Basics/server.js
+++ Basics/server.js
@@ -1,6 +1,10 @@
 //importing in Node.js
+
 const http = require("http");
-const fs = require("fs");
+
+const routes = require("./routes.js");
+
+// const fs = require("fs");
 //const { chdir } = require("process");
 /*const rqListener = (req, res) => {
   console.log(req);
@@ -9,44 +13,10 @@ const fs = require("fs");
 //http.createServer(rqListener);
 // or using the anomymous function
 
-const server = http.createServer(function (req, res) {
-  const url = req.url;
-  const method = req.method;
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter Message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name ="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-    return res.end();
-  }
-  //process.exit(); // or use CTRL + C
-  if (url === "/message" && method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      console.log(chunk);
-      body.push(chunk);
-    });
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      console.log(parsedBody);
-      const message = parsedBody.split("=")[1];
-      //fs.writeFileSync("message.txt", message);
-      fs.writeFile("message.txt", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-        return res.end();
-      });
-    });
-  }
-  res.setHeader("Content-Type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My First Page</title></head>");
-  res.write("<body><h1>Hello from my Node.Js Server!</h1></body>");
-  res.write("</html>");
-  res.end();
-});
+const server = http.createServer(routes)
+  // const url = req.url;
+  // const method = req.method;
+  
 server.listen(5000);
 
 // or using Nextgen function
