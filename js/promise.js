5413ce299f9e7265a45c512aa54857ca49ea8cce
Godfred Nsabo <godfred.nsabo@amalitech.org>
2020-12-02 10:46:50 +0000

    Creating

--- js/promise.js
+++ js/promise.js
@@ -15,25 +15,25 @@ console.log("Hello");
 console.log("Hi");*/
 
 // implementing Promise and Async
-const fetchData = () => {
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve("Completed");
-    }, 150);
-  });
-  return promise;
-};
-setTimeout(() => {
-  console.log("Timer is done!");
-  fetchData()
-    .then((text) => {
-      console.log(text);
-      return fetchData();
-    })
-    .then((text1) => {
-      console.log(text1);
-    });
-}, 300);
+// const fetchData = () => {
+//   const promise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve("Completed");
+//     }, 150);
+//   });
+//   return promise;
+// };
+// setTimeout(() => {
+//   console.log("Timer is done!");
+//   fetchData()
+//     .then((text) => {
+//       console.log(text);
+//       return fetchData();
+//     })
+//     .then((text1) => {
+//       console.log(text1);
+//     });
+// }, 300);
 
-console.log("Hello");
-console.log("Hi");
+// console.log("Hello");
+// console.log("Hi");
